a55c9e7e740cf3ebb470176a0194f66b532053e9
bernie-skipole <bernie@skipole.co.uk>
2021-05-31 11:23:30 +0100

    Drawings edited

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from machine import UART, Pin, ADC
 
 led = Pin(25, Pin.OUT)
+led_state = False
 sensor_temp = ADC(4)
 
 uart = UART(0,baudrate=115200,tx=Pin(0),rx=Pin(1),bits=8,parity=None,stop=1)
@@ -11,16 +12,29 @@ while True:
     value = uart.read(4)
     # all packets are of the 4 byte format [code, d1, d2, 255]
     if value[3] != 255:
+        # not synchronised, read on byte and continue
         uart.read(1)
         continue
+
     # [1, 25, 1, 255] and [1, 25, 0, 255] are turning led on/off
     if (value[0] == 1) and (value[1] == 25):
         # set the led and send the new led state right back
         uart.write(value)
         if value[2] == 1:
             led.value(1)
+            led_state = True
         else:
             led.value(0)
+            led_state = False
+        continue
+
+    # [2, 25, 0, 255] requests current led state
+    if (value[0] == 2) and (value[1] == 25):
+        # get the led state and send back
+        if led_state:
+            uart.write(bytes([2, 25, 1, 255]))
+        else:
+            uart.write(bytes([2, 25, 0, 255]))
         continue
 
     # [3, 0, n, 255] is a monitoring packet, the pi expects it to be echoed back
